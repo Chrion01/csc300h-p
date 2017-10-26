ce58e47baf7c85a9f4643511f3012f472017b4c9
Chrion01 <christonechen@gmail.com>
2017-10-26 00:48:51 -0400

    No idea what I am doing.
    Kinda have an idea of what I'm doing

---
+++ Project.py
@@ -0,0 +1,35 @@
+import sys
+from pycparser import parse_file
+from pycparser.c_ast import *
+from minic.minic_ast import *
+class WhileVisitor(NodeVisitor):
+    def __init__(self):
+        self.nodes = []
+    def visit_While(self, node):
+        self.nodes.append(list(node.children()))
+        node.show()
+
+class ForVisitor(NodeVisitor):
+    def __init__(self):
+        self.nodes = []
+    def visit_For(self, node):
+        self.nodes.append(list(node.children()))
+        node.show()
+
+class ConstantVisitor(NodeVisitor):
+    def __init__(self):
+        self.values = []
+
+    def visit_Constant(self, node):
+        print('Const\n')
+        self.values.append(node.value)
+
+if __name__ == '__main__':
+    ast = parse_file('./tests/c_files/minic.c')
+
+    for_visit = ForVisitor()
+    n_vist = NodeVisitor()
+    #n_vist.visit(ast)
+    for_visit.visit(ast)
+
+    exit(0)
