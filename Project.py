a965bd2488cb81e05935cb4cc3762ab66048754f
Chrion01 <christonechen@gmail.com>
2017-10-28 23:52:28 -0400

    Body Visitor
    A body visitor

--- Project.py
+++ Project.py
@@ -18,12 +18,25 @@ class ForVisitor(NodeVisitor):
 class LoopVisitor(NodeVisitor):
     def __init__(self):
         self.nodes = []
-    def visit_For(self, node):
-        self.nodes.append(list(node.children()))
-        node.show()
-    def visit_While(self, node):
-        self.nodes.append(list(node.children()))
-        node.show()
+    def visit_For(self, For):
+        self.nodes.append(list(For.children()))
+        b_vist = BodyVisitor()
+        b_vist.visit(For)
+
+    def visit_While(self, While):
+        self.nodes.append(list(While.children()))
+        b_vist = BodyVisitor()
+        b_vist.visit(While)
+
+class BodyVisitor(NodeVisitor):
+    def __init__(self):
+        self.values = []
+
+    def visit_Assignment(self, assignment):
+        self.values.append(assignment.lvalue.name)
+        assignment.lvalue.show()
+    def visit_Decl(self, decl):
+        self.values.append(decl.name)
 
 class ConstantVisitor(NodeVisitor):
     def __init__(self):
@@ -40,8 +53,7 @@ if __name__ == '__main__':
     while_visit = WhileVisitor()
     n_vist = LoopVisitor()
     n_vist.visit(ast)
-    #for_visit.visit(ast)
-    #while_visit.visit(ast)
+
 
 
     exit(0)
