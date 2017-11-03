817dd86a6d2e6d846088fc6c04b23e0a835e0429
Chrion01 <christonechen@gmail.com>
2017-11-03 19:02:33 -0400

    Begin live var analysis

--- Project.py
+++ Project.py
@@ -136,16 +136,58 @@ class LiveVarAn(NodeVisitor):
         self.index += 1
 
     def visit_BinaryOp(self, binaryOp):
-        if isinstance(binaryOp.right, pc.ID) and binaryOp.right.name in self.live_vars[self.index]:
-            if not binaryOp.right.name in self.values:
-                self.values.append(binaryOp.right.name)
-        if isinstance(binaryOp.left, pc.ID) and not (binaryOp.left.name in self.decls):
-            if not binaryOp.left.name in self.values:
-                self.values.append(binaryOp.left.name)
+        if isinstance(binaryOp.right, pc.ID):
+            for i in range(0,self.index+1):
+                for var in  self.target_vars[i]:
+                    if(var == binaryOp.right.name):
+                       self.live_vars[i].append(var)
+        if isinstance(binaryOp.left, pc.ID):
+            for i in range(0,self.index+1):
+                for var in  self.target_vars[i]:
+                    if(var == binaryOp.left.name):
+                       self.live_vars[i].append(var)
         else:
             self.visit(binaryOp.left)
             self.visit(binaryOp.right)
 
+    def visit_Assignment(self,node):
+        if isinstance(node.rvalue, pc.ID):
+            for i in range(0, self.index + 1):
+                # for var in self.target_vars[i]:
+                #     if (var == node.rvalue.name):
+                #         self.live_vars[i].append(var)
+                if node.rvalue.name in self.target_vars[i]:
+                    self.live_vars[i].append(node.rvalue.name)
+        else:
+            self.visit(node.rvalue)
+
+        if isinstance(node.lvalue, pc.ID):
+            for i in range(0, self.index + 1):
+                temp = []
+                for var in self.target_vars[i]:
+                    if (var != node.lvalue.name):
+                        temp.append(var)
+                self.target_vars[i] = temp[:]
+
+    def visit_Decl(self,node):
+        if not isinstance(node.type, pc.FuncDecl) and not isinstance(node.type, pc.PtrDecl):
+            if isinstance(node.init, pc.ID):
+                for i in range(0, self.index + 1):
+                    for var in self.target_vars[i]:
+                        if (var == node.init.name):
+                            self.live_vars[i].append(node.init.name)
+            elif isinstance(node.init, pc.Constant):
+                pass
+            else:
+                self.visit(node.init)
+
+        if isinstance(node.name, pc.ID):
+            for i in range(0, self.index + 1):
+                temp = []
+                for var in self.target_vars[i]:
+                    if (var != node.name):
+                        temp.append(var)
+                self.target_vars[i] = temp[:]
 
 
 
@@ -154,12 +196,7 @@ class LiveVarAn(NodeVisitor):
 
 
 
-
-
-
-
-
-    # def visit_
+                # def visit_
 
 class FuncVisitor(NodeVisitor):
 
@@ -183,7 +220,11 @@ class FuncVisitor(NodeVisitor):
         rl.visit(FuncDecl)
         # combination = [rl.loop_reach_definition, rl.expressions_after]
         self.function_reach_defs.append(rl.loop_reach_definition)
-        self.function_live_vars.append(rl.expressions_after)
+
+        lv = LiveVarAn(lv.loop_vars, lv.nodes)
+        lv.visit(FuncDecl)
+        self.function_live_vars.append(lv.live_vars)
+
 
 
 
@@ -197,17 +238,18 @@ if __name__ == '__main__':
     print("loops    : {}".format(n_vist.function_nodes))
     print("loop vars: {}".format(n_vist.function_loop_variables))
     print("loop reach vars: {}".format(n_vist.function_reach_defs))
-    for function in n_vist.function_reach_defs:
-        print("f")
-        for loop in function:
-            print('l')
-            for exp in loop:
-                if isinstance(exp, pc.Decl):
-                    print('Decl: {} = {}'.format(exp.name, exp.init.value))
-                elif isinstance(exp, pc.Assignment):
-                    print('Asgn: {}'.format(exp.lvalue.name))
+    print("loop live vars: {}".format(n_vist.function_live_vars))
+    # for function in n_vist.function_reach_defs:
+    #     print("f")
+    #     for loop in function:
+    #         print('l')
+    #         for exp in loop:
+    #             if isinstance(exp, pc.Decl):
+    #                 print('Decl: {} = {}'.format(exp.name, exp.init.value))
+    #             elif isinstance(exp, pc.Assignment):
+    #                 print('Asgn: {}'.format(exp.lvalue.name))
 
-    ast.show()
+    # ast.show()
 
     # loop_var_reach = LoopReach(n_vist.loop_vars, n_vist.nodes)
     # loop_var_reach.visit(ast)
