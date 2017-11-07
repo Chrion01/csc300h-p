217c04802721e510ff1dcebcfd6c69ec0482be1d
Chrion01 <christonechen@gmail.com>
2017-11-07 16:45:15 -0500

    Combine data into Loop class. Array index variable modifiers

--- Project.py
+++ Project.py
@@ -2,50 +2,91 @@ import sys
 from pycparser import parse_file
 import pycparser.c_ast as pc
 from minic.minic_ast import *
+from minic.c_ast_to_minic import *
 
 
 class LoopVisitor(NodeVisitor):
+
     def __init__(self):
         self.nodes = []
         self.loop_vars = []
+        self.reads = []
+        self.writes = []
+        self.array_index = []
 
     def visit_For(self, For):
-
         self.nodes.append(For)
         b_vist = BodyVisitor()
         b_vist.visit(For)
         self.loop_vars.append(b_vist.values[:])
+        self.reads.append(b_vist.reads[:])
+        self.writes.append(b_vist.writes[:])
+        array_in_mod = FindModifiers(b_vist.array_indexes)
+        array_in_mod.visit(For)
+        self.array_index.append(array_in_mod.modifiers)
 
     def visit_While(self, While):
         self.nodes.append(While)
         b_vist = BodyVisitor()
         b_vist.visit(While)
         self.loop_vars.append(b_vist.values[:])
+        self.reads.append(b_vist.reads[:])
+        self.writes.append(b_vist.writes[:])
+        array_in_mod = FindModifiers(b_vist.array_indexes)
+        array_in_mod.visit(While)
+        self.array_index.append(array_in_mod.modifiers)
 
 class BodyVisitor(NodeVisitor):
 
     def __init__(self):
         self.values = []
         self.decls = []
+        self.reads = []
+        self.writes = []
+        self.array_indexes = []
 
     def visit_Assignment(self, assignment):
-        if not assignment.lvalue.name in self.values:
-            self.values.append(assignment.lvalue.name)
+        if isinstance(assignment.lvalue, pc.ID):
+            if not assignment.lvalue.name in self.values:
+                self.values.append(assignment.lvalue.name)
+                self.writes.append(assignment.lvalue.name)
+        elif isinstance(assignment.lvalue, pc.ArrayRef):
+            self.writes.append(assignment.lvalue.name.name)
+            self.array_indexes.append(assignment.lvalue.subscript.name)
+        if isinstance(assignment.rvalue, pc.ID):
+            self.reads.append(assignment.rvalue.name)
+        elif isinstance(assignment.rvalue, pc.ArrayRef):
+            self.reads.append(assignment.rvalue.name.name)
+        else:
+            self.visit(assignment.rvalue)
+
 
     def visit_BinaryOp(self, binaryOp):
-        if isinstance(binaryOp.right, pc.ID) and not(binaryOp.right.name in self.decls):
-            if not binaryOp.right.name in self.values:
+        if isinstance(binaryOp.right, pc.ID):
+            if binaryOp.right.name not in self.values and not(binaryOp.right.name in self.decls):
                 self.values.append(binaryOp.right.name)
-        if isinstance(binaryOp.left, pc.ID) and not(binaryOp.left.name in self.decls):
-            if not binaryOp.left.name in self.values:
+                self.reads.append(binaryOp.right.name)
+        else:
+            self.visit(binaryOp.right)
+        if isinstance(binaryOp.left, pc.ID):
+            if binaryOp.left.name not in self.values and not(binaryOp.left.name in self.decls):
                 self.values.append(binaryOp.left.name)
+                self.reads.append(binaryOp.left.name)
         else:
             self.visit(binaryOp.left)
-            self.visit(binaryOp.right)
 
     def visit_Decl(self, decl):
         if not decl.name in self.decls:
             self.decls.append(decl.name)
+
+    def visit_UnaryOp(self, unary):
+        if isinstance(unary.expr, pc.ID) and not(unary.expr.name in self.decls):
+            self.values.append(unary.expr.name)
+            self.writes.append(unary.expr.name)
+
+
+
+
 
 # class ConstantVisitor(NodeVisitor):
 #     def __init__(self):
@@ -222,7 +263,22 @@ class LiveVarAn(NodeVisitor):
                     self.target_vars[i] = temp[:]
     
     
-    
+class FindModifiers(NodeVisitor):
+
+    def __init__(self, variables):
+        self.vars = variables
+        self.modifiers = {}
+        for var in self.vars:
+            self.modifiers[var] = []
+
+    def visit_UnaryOp(self, unary):
+        if isinstance(unary.expr, pc.ID) and unary.expr.name in self.vars:
+            self.modifiers[unary.expr.name].append(unary)
+
+    def visit_Assignment(self, assignment):
+        if isinstance(assignment.lvalue, pc.ID) and assignment.lvalue.name in self.vars:
+            self.modifiers[assignment.lvalue.name].append(assignment)
+
 
 
 
@@ -239,6 +295,7 @@ class FuncVisitor(NodeVisitor):
         self.function_reach_defs = []
         self.function_live_vars = []
 
+        self.loops = []
 
     def visit_FuncDef(self, FuncDecl):
         self.functions.append((FuncDecl.decl.name,FuncDecl))
@@ -257,20 +314,52 @@ class FuncVisitor(NodeVisitor):
         lva.visit(FuncDecl)
         self.function_live_vars.append(lva.live_vars[:])
 
+        for i in range(len(lv.nodes)):
+            loop = Loop(lv.nodes[i], lv.reads[i], lv.writes[i],
+                        rl.loop_reach_definition[i], lva.live_vars[i], lv.array_index[i])
+            self.loops.append(loop)
 
 
+class Loop():
+    def __init__(self, node, reads, writes, defs, live, array_ind):
+        self.node = node
+        self.read_variables = reads
+        self.write_variables = writes
+        self.reach_defs = defs
+        self.live_vars = live
+        self.array_ind = array_ind
 
+    def __str__(self):
+        defs = []
+        for item in self.reach_defs:
+            print(str(item))
+            defs.append(transform(item).__str__())
+
+        statement = "Read Variables: {}\n".format(self.read_variables) + \
+                    "Write Variables: {}\n".format(self.write_variables) + \
+                    "Reaching Def: {}\n".format(defs) + \
+                    "Live Variables: {}\n".format(self.live_vars)
+        for key in self.array_ind.keys():
+            statement += "Array Index Var {}\n".format(key)
+            for item in self.array_ind[key]:
+                statement += "{}\n".format(transform(item).__str__())
+
+
+        return statement
 
 if __name__ == '__main__':
     ast = parse_file('./tests/c_files/minic.c')
 
     n_vist = FuncVisitor()
     n_vist.visit(ast)
-    print("functions: {}".format(n_vist.functions))
-    print("loops    : {}".format(n_vist.function_nodes))
-    print("loop vars: {}".format(n_vist.function_loop_variables))
-    print("loop reach vars: {}".format(n_vist.function_reach_defs))
-    print("loop live vars: {}".format(n_vist.function_live_vars))
+    # print("functions: {}".format(n_vist.functions))
+    # print("loops    : {}".format(n_vist.function_nodes))
+    # print("loop vars: {}".format(n_vist.function_loop_variables))
+    # print("loop reach vars: {}".format(n_vist.function_reach_defs))
+    # print("loop live vars: {}".format(n_vist.function_live_vars))
+    for loop in n_vist.loops:
+        print(loop)
+
     # for function in n_vist.function_reach_defs:
     #     print("f")
     #     for loop in function:
@@ -281,7 +370,7 @@ if __name__ == '__main__':
     #             elif isinstance(exp, pc.Assignment):
     #                 print('Asgn: {}'.format(exp.lvalue.name))
 
-    # ast.show()
+    ast.show()
 
     # loop_var_reach = LoopReach(n_vist.loop_vars, n_vist.nodes)
     # loop_var_reach.visit(ast)
